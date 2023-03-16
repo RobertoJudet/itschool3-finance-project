5ff32fee8f18f4d1bdd6d44482183fa4ad5cc274
Raluca <paraschivraluca@gmail.com>
2023-03-16 21:05:03 +0200

    Add domain user class

---
+++ finance-project/api/users.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter
+
+
+users_router = APIRouter(prefix="/users")
+
+
+@users_router.get("/")
+def get_all_users():
+    return []
+
+
+@users_router.post("/")
+def create_a_user():
+    pass
---
+++ tests/user/test_user.py
@@ -0,0 +1,26 @@
+import unittest
+
+from domain.user.user import User
+
+
+class UserTestCase(unittest.TestCase):
+    def test_user_sets_the_right_username(self):
+        #set up
+        username = "random generate"
+        user = User(username)
+        # execution
+        actual_username = user.username
+
+        #assertion
+        self.assertEqual(username, actual_username)
+
+    @unittest.skip("TODO homework")
+    def test_it_sets_empty_list_if_we_do_not_specify_stock(self):
+
+    @unittest.skip("TODO Homerok")
+    def test_it_sets_the_stock_we_give(self):
+        # set  a list of 3 strings
+
+
+if __name__ == '__main__':
+    unittest.main()
