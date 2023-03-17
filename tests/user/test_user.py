cab2d70b1fcaa48941d824df785569bd7acbba71
Raluca <paraschivraluca@gmail.com>
2023-03-17 17:22:15 +0200

    Add test stocks

--- tests/user/test_user.py
+++ tests/user/test_user.py
@@ -1,5 +1,6 @@
 import unittest
 
+from domain.user import user
 from domain.user.user import User
 
 
@@ -14,13 +15,26 @@ class UserTestCase(unittest.TestCase):
         #assertion
         self.assertEqual(username, actual_username)
 
-    @unittest.skip("TODO homework")
+
     def test_it_sets_empty_list_if_we_do_not_specify_stock(self):
+        # set up
+        stocks = []
+        stock = User(stocks)
+        # execution
+        actual_stock = stock.stocks
+        # assertion
+        self.assertEqual(stocks, actual_stock)
 
 
-    @unittest.skip("Homework")
     def test_it_sets_the_stock_we_give(self):
         # set  a list of 3 strings
+        # set up
+        stocks = ["user1", "user2", "user3"]
+        stock = User(stocks)
+        # execution
+        actual_stock = stock.stocks
+        # assertion
+        self.assertNotEqual(stocks, actual_stock)
 
 
 if __name__ == '__main__':
