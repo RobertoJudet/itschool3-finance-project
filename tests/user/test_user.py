8f832d0850158ed4f1f5b294bf7641ab460ec156
Raluca <paraschivraluca@gmail.com>
2023-03-21 20:00:47 +0200

    Add factory for user

--- tests/user/test_user.py
+++ tests/user/test_user.py
@@ -18,15 +18,14 @@ class UserTestCase(unittest.TestCase):
 
     def test_it_sets_empty_list_if_we_do_not_specify_stock(self):
         # set up
-        stocks = []
-        stock = User(stocks)
+        user = User("random-username")
         # execution
-        actual_stock = stock.stocks
+        actual_stocks = user.stocks
         # assertion
-        self.assertEqual(stocks, actual_stock)
+        self.assertEqual([], actual_stocks)
 
 
-    def test_it_sets_the_stock_we_give(self):
+    def test_it_sets_the_stock_we_give(self): ("TODO Homework")
         # set  a list of 3 strings
         # set up
         stocks = ["user1", "user2", "user3"]
@@ -36,6 +35,11 @@ class UserTestCase(unittest.TestCase):
         # assertion
         self.assertNotEqual(stocks, actual_stock)
 
+    def test_for_repo(self):
+        pass
+
+    def test_for_factory(self):
+        pass
 
 if __name__ == '__main__':
     unittest.main()
