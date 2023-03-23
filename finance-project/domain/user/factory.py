02c9e061ab733cbc1c4dc8d623492e225db1bcdd
Raluca <paraschivraluca@gmail.com>
2023-03-23 18:39:05 +0200

    Add test factory

--- finance-project/domain/user/factory.py
+++ finance-project/domain/user/factory.py
@@ -1,22 +1,19 @@
-import uuid
 from domain.user.user import User
-
 
 class InvalidUsername(Exception):
     pass
 
-
 class UserFactory:
-    # username should be at least 6 chars and max 20 chars, it can only contain letters, numbers & -
-    def make_new(self, username: str) -> User:
-        # TODO rest of validations
-        if len(username) < 6:
-            raise InvalidUsername("Username should have at least 6 characters")
-        user_uuid = uuid.uuid4()
-        return User(user_uuid, username)
 
-    def make_from_persistance(self, info: tuple) -> User:
-        return User(
-            uuid=uuid.UUID(info[0]),
-            username=info[1],
-        )
+    def make(self, username: str) -> User:
+        for i in username:
+            if not (i.isalnum() or i == "-"):
+                raise InvalidUsername("User is not valid")
+            else:
+                raise InvalidUsername("User is valid")
+        if len(username) < 6 or len(username) > 20:
+            raise InvalidUsername("Username should have at least 6 characters or maximum 20 characters")
+        else:
+            return User(username)
+
+
--- tests/domain/user/test_factory.py
+++ tests/domain/user/test_factory.py
@@ -1,42 +1,66 @@
 import unittest
-
 from domain.user.factory import UserFactory, InvalidUsername
 from domain.user.user import User
 
 
-# TODO update tests
 class UserFactoryTestCase(unittest.TestCase):
-    def test_it_creates_a_user_if_the_username_is_between_6_and_20_chars(self):
-        username = "between-6-and-20"
+    def test_it_creates_user_if_the_username_is_between_6_and_20_chars(self):
+        username = "between-6-20"
         factory = UserFactory()
 
-        actual_user = factory.make_new(username)
+        actual_user = factory.make(username)
 
         self.assertEqual(username, actual_user.username)
         self.assertEqual(User, type(actual_user))
 
-    def test_it_raises_exception_if_the_username_is_below_6_chars(self):
+    def test_it_raises_exception_if_the_username_is_bellow_6_chars(self):
         username = "below"
         factory = UserFactory()
 
         with self.assertRaises(InvalidUsername) as context:
-            factory.make_new(username)
+            factory.make(username)
 
         self.assertEqual(
-            "Username should have at least 6 characters", str(context.exception)
+            "Username should have at least 6 characters or maximum 20 characters",
+            str(context.exception),
         )
 
-    @unittest.skip("TODO")
-    def test_it_raises_exception_if_the_username_is_above_20_chars(self):
-        pass
+    def test_it_rasises_exception_if_the_username_is_above_20_chars(self):
+        username = "above"
+        factory = UserFactory()
 
-    @unittest.skip("TODO")
+        with self.assertRaises(InvalidUsername) as context:
+            factory.make(username)
+
+        self.assertEqual(
+            "Username should have at least 6 characters or maximum 20 characters",
+            str(context.exception),
+        )
+
+
     def test_it_creates_a_user_if_the_username_has_valid_chars(self):
-        pass
+        username = "raluca"
+        factory = UserFactory()
 
-    @unittest.skip("TODO")
-    def test_it_raises_exception_if_the_username_has_invalid_chars(self):
-        pass
+        with self.assertRaises(InvalidUsername) as context:
+            factory.make(username)
+
+        self.assertEqual(
+            "User is valid", str(context.exception)
+        )
+
+
+    def test_if_raises_exception_if_the_username_has_invalid_chars(self):
+        username = "raluca+"
+        factory = UserFactory()
+
+        with self.assertRaises(InvalidUsername) as context:
+            factory.make(username)
+
+        self.assertEqual(
+            "User is not valid", str(context.exception)
+        )
+
 
 
 if __name__ == "__main__":
