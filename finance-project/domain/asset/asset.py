0e1d1a1edf656c66c27905f645409813e5677540
RobertoJudet <roberto.judet@itschool.ro>
2023-03-28 20:58:10 +0300

    Add asset to domain

--- finance-project/domain/asset/asset.py
+++ finance-project/domain/asset/asset.py
@@ -1,6 +1,3 @@
-import yfinance
-
-
 class Asset:
     def __init__(self, ticker: str, nr: float, name: str, country: str, sector: str):
         self.__ticker = ticker
@@ -8,61 +5,3 @@ class Asset:
         self.__name = name
         self.__country = country
         self.__sector = sector
-        yfin = yfinance.Ticker(ticker)
-        self.__info = yfin.fast_info
-
-    @property
-    def ticker(self) -> str:
-        return self.__ticker
-
-    @property
-    def units(self) -> float:
-        return self.__nr
-
-    @property
-    def name(self) -> str:
-        return self.__name
-
-    @property
-    def country(self) -> str:
-        return self.__country
-
-    @property
-    def sector(self) -> str:
-        return self.__sector
-
-    @property
-    def current_price(self) -> float:
-        price = self.__info["lastPrice"]
-        return round(price, 2)
-
-    @property
-    def currency(self) -> str:
-        return self.__info["currency"]
-
-    @property
-    def close_price(self) -> float:
-        return self.__info["previousClose"]
-
-    @property
-    def fifty_day_price(self) -> float:
-        return self.__info["fiftyDayAverage"]
-
-    @property
-    def today_low_price(self) -> float:
-        return self.__info["dayLow"]
-
-    @property
-    def today_high_price(self) -> float:
-        return self.__info["dayHigh"]
-
-    @property
-    def open_price(self) -> float:
-        return self.__info["open"]
-
-    @property
-    def evolution(self):
-        evolution = ((
-                             self.__info["lastPrice"] - self.__info["previousClose"]
-                     ) / self.__info["previousClose"]) * 100
-        return evolution
--- finance-project/domain/asset/factory.py
+++ finance-project/domain/asset/factory.py
@@ -4,9 +4,10 @@ from domain.asset.asset import Asset
 
 class AssetFactory:
     def make_new(self, ticker: str) -> Asset:
+        # TODO error handling & tests
         t = yahooquery.Ticker(ticker)
         profile = t.summary_profile[ticker]
-        name = self.__extract_name(profile)
+        name = profile["longBusinessSummary"].split(" ")[0:2]
         country = profile["country"]
         sector = profile["sector"]
         return Asset(
@@ -16,11 +17,3 @@ class AssetFactory:
             country=country,
             sector=sector,
         )
-
-    @staticmethod
-    def __extract_name(profile: dict) -> str:
-        summary = profile["longBusinessSummary"]
-        words = summary.split(" ")
-        first_2_words = words[0:2]
-        name = " ".join(first_2_words)
-        return name
