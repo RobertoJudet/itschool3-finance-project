6ae77d47afaffcb21a3edb5cadbf1bd358122a9e
Raluca <paraschivraluca@gmail.com>
2023-03-28 21:05:54 +0300

    Update requirements

--- finance-project/main.py
+++ finance-project/main.py
@@ -1,15 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from api.users import users_router
+from domain.user.factory import InvalidUsername
+from starlette.responses import JSONResponse
+from api.assets import assets_router
 
 app = FastAPI(
     debug=True,
     title="Fintech Portfolio API",
     description="A webserver with a REST API for keeping track of your different financial assets,"
-    " stock & crypto, and see/compare their evolution",
-    version="0.1.0",
+                " stock & crypto, and see/compare their evolution",
+    version="0.3.0",
 )
 
 app.include_router(users_router)
+app.include_router(assets_router)
+
+
+@app.exception_handler(InvalidUsername)
+def return_invalid_username(_: Request, e: InvalidUsername):
+    return JSONResponse(status_code=400, content="Username is not valid! Error: " + str(e))
+
 
 if __name__ == "__main__":
     import subprocess
