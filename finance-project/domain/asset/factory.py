ca5ea14fe4bc12d3d5448e670a3a379a4bded73b
Raluca <paraschivraluca@gmail.com>
2023-03-28 20:57:46 +0300

    Add asset to domain

--- finance-project/domain/asset/factory.py
+++ finance-project/domain/asset/factory.py
@@ -3,12 +3,10 @@ from domain.asset.asset import Asset
 
 
 class AssetFactory:
-    # TODO unit tests for it
     def make_new(self, ticker: str) -> Asset:
-        # TODO error handling & tests
         t = yahooquery.Ticker(ticker)
         profile = t.summary_profile[ticker]
-        name = self.__extract_name(profile)
+        name = profile["longBusinessSummary"].split(" ")[0:2]
         country = profile["country"]
         sector = profile["sector"]
         return Asset(
@@ -18,11 +16,3 @@ class AssetFactory:
             country=country,
             sector=sector,
         )
-
-    # TODO make it class or static method, why? we don't use self
-    def __extract_name(self, profile: dict) -> str:
-        summary = profile["longBusinessSummary"]
-        words = summary.split(" ")
-        first_2_words = words[0:2]
-        name = " ".join(first_2_words)
-        return name
