57b7b893d7f265d05148f18457d94bc5ca51a716
Alexandru <alex_moldovan2000@yahoo.com>
2023-04-04 19:37:37 +0300

    Refactor query in asset class

--- finance-project/domain/asset/asset.py
+++ finance-project/domain/asset/asset.py
@@ -8,7 +8,8 @@ class Asset:
         self.__name = name
         self.__country = country
         self.__sector = sector
-        self.__yfin = yfinance.Ticker(ticker)
+        yfin = yfinance.Ticker(ticker)
+        self.__info = yfin.fast_info
 
     @property
     def ticker(self) -> str:
@@ -28,20 +29,38 @@ class Asset:
 
     @property
     def current_price(self) -> float:
-        price = self.__yfin.fast_info["lastPrice"]
+        price = self.__info["lastPrice"]
         return round(price, 2)
 
     @property
     def currency(self) -> str:
-        return self.__yfin.fast_info["currency"]
+        return self.__info["currency"]
 
     @property
     def close_price(self) -> float:
-        return self.__yfin.fast_info["previousClose"]
+        return self.__info["previousClose"]
 
     @property
     def fifty_day_price(self) -> float:
-        return self.__yfin.fast_info["fiftyDayAverage"]
+        return self.__info["fiftyDayAverage"]
 
-    # TODO a property, in percentage how much it went up or down
-    # lastPrice(currentPrice) & closed price
+    @property
+    def today_low_price(self) -> float:
+        return self.__info["dayLow"]
+
+    @property
+    def today_high_price(self) -> float:
+        return self.__info["dayHigh"]
+
+    @property
+    def open_price(self) -> float:
+        return self.__info["open"]
+
+    @property
+    def up_or_down_percentage(self) -> float:
+        x = self.__info["open"]
+        y = self.__info["previousClose"]
+        if x > y:
+            return -x / y
+        else:
+            return x / y
