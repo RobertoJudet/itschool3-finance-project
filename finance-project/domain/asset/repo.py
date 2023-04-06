59e29460132b652e792aa1e4daf59d9aa18cc5a9
RobertoJudet <roberto.judet@itschool.ro>
2023-04-06 23:41:19 +0300

    Add api endpoint to insert stocks into a user's portfolio

--- finance-project/domain/asset/repo.py
+++ finance-project/domain/asset/repo.py
@@ -5,6 +5,9 @@ from domain.user.user import User
 
 class AssetRepo:
     def add_to_user(self, user: User, asset: Asset):
+        # TODO homework, what happens if we already have this asset?
+        # sqlite3.IntegrityError: UNIQUE constraint failed: e12bb836_7a3d_4cbc_9253_15179932fc40_assets.ticker
+        # exception, 400 to api already added
         table = f"{user.id}-assets".replace("-", "_")
         with sqlite3.connect(f"main_users.db") as conn:
             cursor = conn.cursor()
@@ -25,16 +28,13 @@ class AssetRepo:
         table = f"{user.id}-assets".replace("-", "_")
         with sqlite3.connect(f"main_users.db") as conn:
             cursor = conn.cursor()
-            try:
-                cursor.execute(f"SELECT * FROM '{table}'")
-            except sqlite3.OperationalError as e:
-                if "no such table" in str(e):
-                    return []
-                else:
-                    raise e
+            cursor.execute(f"SELECT * FROM '{table}'")
             assets_info = cursor.fetchall()
-        assets = [
-            Asset(ticker=x[0], nr=x[3], name=x[1], country=x[2], sector="sec")
-            for x in assets_info
-        ]
+        assets = [Asset(
+            ticker=x[0],
+            nr=x[3],
+            name=x[1],
+            country=x[2],
+            sector="sec"
+        ) for x in assets_info]
         return assets
