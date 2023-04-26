65ab309ff41fc0cfc15052ff85fd7b75709d4dee
Alexandru <alex_moldovan2000@yahoo.com>
2023-04-26 17:21:56 +0300

    Add delete & get_by_id

--- finance-project/persistence/user_sqlite.py
+++ finance-project/persistence/user_sqlite.py
@@ -1,38 +1,42 @@
 import sqlite3
-from domain.user.persistance_interface import UserPersistenceInterface
+from domain.user.persistence_interface import UserPersistenceInterface
 from domain.user.user import User
 from domain.user.factory import UserFactory
 
 
 class UserPersistenceSqlite(UserPersistenceInterface):
+
     def get_all(self) -> list[User]:
         with sqlite3.connect("main_users.db") as conn:
             cursor = conn.cursor()
-            # TODO try except, return empty list if no db
             try:
                 cursor.execute("SELECT * FROM users")
-            except:
-                return []
+            except sqlite3.OperationalError as e:
+                if "no such table" in str(e):
+                    return []
+                else:
+                    raise e
             users_info = cursor.fetchall()
-        factory = UserFactory()
-        users = [factory.make_from_persistance(x) for x in users_info]
+            factory = UserFactory()
+            users = [factory.make_from_persistence(x) for x in users_info]
+
         return users
 
     def add(self, user: User):
         with sqlite3.connect("main_users.db") as conn:
             cursor = conn.cursor()
             try:
-                cursor.execute(
-                    f"INSERT INTO users (id, username) VALUES ('{user.id}','{user.username}')"
-                )
+                cursor.execute(f"INSERT INTO users (id, username) VALUES ('{user.id}', '{user.username}')")
             except sqlite3.OperationalError as e:
                 if "no such table" in str(e):
-                    cursor.execute(
-                        "CREATE TABLE users (id TEXT PRIMARY KEY, username TEXT NOT NULL)"
-                    )
+                    cursor.execute("CREATE TABLE users (id TEXT PRIMARY KEY, username TEXT NOT NULL)")
                 else:
                     raise e
-                cursor.execute(
-                    f"INSERT INTO users (id, username) VALUES ('{user.id}','{user.username}')"
-                )
+                cursor.execute(f"INSERT INTO users (id, username) VALUES ('{user.id}', '{user.username}')")
             conn.commit()
+
+    def delete_by_id(self, uid: str):
+        pass
+
+    def get_by_id(self, uid: str) -> User:
+        pass
