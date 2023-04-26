598be1d30784c4371e8d9b73af66c9eef7af9dce
Alexandru <alex_moldovan2000@yahoo.com>
2023-04-26 17:25:15 +0300

    Add InvalidPersistence

--- finance-project/api/users.py
+++ finance-project/api/users.py
@@ -1,46 +1,9 @@
-# from fastapi import APIRouter
-# from api.models import UserAdd, UserInfo
-# from domain.user.factory import UserFactory
-# from domain.user.repository import UserRepo
-#
-#
-# users_router = APIRouter(prefix="/users")
-# repo = UserRepository()
-#
-#
-# @users_router.get("", response_model=list[UserInfo])
-# def get_all_users():
-#     users = repo.get_all()
-#     with open("usernames.txt", "w") as f:
-#         for user in users:
-#             f.write(user.username + "\n")
-#     return [UserInfo(name=u.username, stocks=u.stocks) for u in users]
-#
-#
-# @users_router.delete("/")
-# def delete_user(username: str):
-#     repo.delete(username)
-#
-#
-# @users_router.post("", response_model=UserInfo)
-# def create_a_user(user: UserAdd):
-#     new_user = UserFactory.create(user.name)
-#     repo.add(new_user)
-#     return UserInfo(name=new_user.username, stocks=new_user.stocks)
-
-# Homework 1 for Project
-# implement get, create and delete user in domain too (user repo & user factory)
-# also create api models
-# create tests for repo & factory
-# username should be at least 6 chars and max 20 chars, it can only contain letter, numbers & -
-# save the user list in a file
-
 from fastapi import APIRouter, Depends
-
+import json
 from domain.asset.factory import AssetFactory
 from domain.asset.repo import AssetRepo
-from domain.user.repo import UserRepo
 from domain.user.factory import UserFactory
+from domain.user.repo import UserRepo
 from api.models import UserAdd, UserInfo, AssetInfoUser, AssetAdd
 from persistence.user_file import UserPersistenceFile
 from persistence.user_sqlite import UserPersistenceSqlite
@@ -48,47 +11,73 @@ from persistence.user_sqlite import UserPersistenceSqlite
 users_router = APIRouter(prefix="/users")
 
 
+class InvalidPersistenceType(Exception):
+    pass
+
+
+def check_persistence_type(file_path):
+    with open(file_path, "r") as config_info:
+        persistence_type = config_info.read()
+        data = json.loads(persistence_type)
+        if "sqlite" in str(data["persistence"]):
+            return UserPersistenceSqlite()
+        elif "json" in str(data["persistence"]):
+            return UserPersistenceFile("main_users.json")
+        else:
+            raise InvalidPersistenceType(
+                "Unrecognized persistence config type. Please check config.json file."
+            )
+
+
 def get_user_repo() -> UserRepo:
-    #  user_persistence = UserPersistenceFile("main_user.json")
-    user_persistence = UserPersistenceSqlite()
+    user_persistence = check_persistence_type("config.json")
+    # user_persistence = UserPersistenceFile("main_users.json")
+    # user_persistence = UserPersistenceSqlite()
     return UserRepo(user_persistence)
 
 
-# Homework 1 for Project
-# implement get, create and delete user in domain too (user repo & user factory)
-# also create api models
-# create tests for repo & factory
-# username should be at least 6 chars and max 20 chars, it can only contain letter, numbers & -
-# save the user list in a file
-
-# TODO GET /users/{user_id}
-
-
-@users_router.get("", response_model=list[UserInfo], )
+@users_router.get("", response_model=list[UserInfo])
 def get_all_users(repo=Depends(get_user_repo)):
     return repo.get_all()
 
 
 @users_router.get("/{user_id}", response_model=UserInfo)
-def get_user(user_id: str, repo=Depends(get_user_repo)):
+def get_user_by_id(user_id: str, repo=Depends(get_user_repo)):
     return repo.get_by_id(user_id)
 
 
+# @users_router.get("/{username}", response_model=UserInfo)
+# def get_user(username: str):
+# return repo.get_by_username(username)
+
+
+@users_router.delete("/{user_id}")
+def delete_user(user_id: str, repo=Depends(get_user_repo)):
+    repo.delete_by_id(user_id)
+
+
 @users_router.post("", response_model=UserInfo)
-def create_a_user(new_user: UserAdd,repo=Depends(get_user_repo)):
+def create_a_user(new_user: UserAdd, repo=Depends(get_user_repo)):
     user = UserFactory().make_new(new_user.username)
     repo.add(user)
     return user
 
 
-# TODO delete a user, DELETE /users/{user_id}
+@users_router.put("/{user_id}", response_model=UserInfo)
+def edit_by_id(user_id: str, username: str, repo=Depends(get_user_repo)):
+    repo.edit(user_id, username)
+    return repo.get_by_id(user_id)
 
 
-# TODO fix api return asset info
-@users_router.post("/{user_id}/asset", response_model=AssetInfoUser)
+# TODO fix api, return asset info
+@users_router.post("/{user_id}/assets", response_model=AssetInfoUser)
 def add_asset_to_user(user_id: str, asset: AssetAdd, repo=Depends(get_user_repo)):
     new_asset = AssetFactory().make_new(asset.ticker)
+    # TODO homework, if asset exception throw 400/404
     user = repo.get_by_id(user_id)
-    user.add_stock(new_asset)
+    # TODO homework, check we have a user otherwise throw exception code 404
+    # user.add_stock(new_asset)
+    print(user)
     AssetRepo().add_to_user(user, new_asset)
+
     return new_asset
