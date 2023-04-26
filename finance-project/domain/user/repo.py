d95828e48bb98796da586d834c8dcafe098c59c4
Alexandru <alex_moldovan2000@yahoo.com>
2023-04-26 17:13:34 +0300

    Add edit for repo

--- finance-project/domain/user/repo.py
+++ finance-project/domain/user/repo.py
@@ -1,7 +1,6 @@
 import json
 import uuid
 from singleton import singleton
-
 from domain.asset.repo import AssetRepo
 from domain.user.factory import UserFactory
 from domain.user.persistance_interface import UserPersistenceInterface
@@ -11,26 +10,34 @@ from domain.user.user import User
 @singleton
 class UserRepo:
     def __init__(self, persistence: UserPersistenceInterface):
-        print("Init user repo ")
+        print("Init user repo")
         self.__persistence = persistence
         self.__users = None
 
     def add(self, new_user: User):
-        #  TODO, refactor do not have duplicate (add, get_all) + add for get_by_id
-        if self.__users is None:
-            self.__users = self.__persistence.get_all()
+        self.__check_we_have_users()
         self.__persistence.add(new_user)
         self.__users.append(new_user)
 
     def get_all(self) -> list[User]:
-        if self.__users is None:
-            self.__users = self.__persistence.get_all()
+        self.__check_we_have_users()
         return self.__users
 
     def get_by_id(self, uid: str) -> User:
+        self.__check_we_have_users()
+        return self.__persistence.get_by_id(uid)
+
+    def delete_by_id(self, uid: str):
+        self.__check_we_have_users()
+        self.__persistence.delete_by_id(uid)
+
+    def edit(self, user_id: str, username: str):
+        self.__check_we_have_users()
+        self.__persistence.edit(user_id, username)
+        for user in self.__users:
+            if user.id == uuid.UUID(hex=user_id):
+                user.username = username
+
+    def __check_we_have_users(self):
         if self.__users is None:
             self.__users = self.__persistence.get_all()
-        for u in self.__users:
-            if u.id == uuid.UUID(hex=uid):
-                assets = AssetRepo().get_for_user(u)
-                return User(uuid=u.id, username=u.username, stocks=assets)
