aa69cdf4cb5141a7d4f873732760568130264a46
ralucapetrea76 <paraschivraluca@gmail.com>
2023-05-11 19:48:11 +0300

    add persistence

---
+++ domain/user/persistence_interface.py
@@ -0,0 +1,21 @@
+import abc
+
+from domain.user.user import User
+
+
+class UserPersistenceInterface(abc.ABC):
+    @abc.abstractmethod
+    def add(self, user: User):
+        pass
+
+    @abc.abstractmethod
+    def get_all(self) -> list[User]:
+        pass
+
+
+    def edit_by_id(self, user_id: User.id, username: str):
+        pass
+
+
+    def delete_by_id(self, uuid: int):
+        pass
