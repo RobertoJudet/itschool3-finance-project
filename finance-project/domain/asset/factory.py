9428e6c3fdf060fd61d885f63dc2c83e0f6c9f21
Alexandru <alex_moldovan2000@yahoo.com>
2023-05-11 16:43:38 +0300

    Updates

--- finance-project/domain/asset/factory.py
+++ finance-project/domain/asset/factory.py
@@ -2,11 +2,17 @@ from domain.asset.asset import Asset
 import yahooquery
 
 
+class InvalidTicker(Exception):
+    pass
+
+
 class AssetFactory:
     def make_new(self, ticker: str) -> Asset:
         # TODO error handling&tests
         t = yahooquery.Ticker(ticker)
         profile = t.summary_profile[ticker]
+        if type(profile) not in [dict]:
+            raise InvalidTicker(f"Invalid ticker {ticker}")
         name = self.__extract_name(profile)
         country = profile["country"]
         sector = profile["sector"]
