741777aa1a0ca76efe2976d8522122f6de8c1984
Alexandru <alex_moldovan2000@yahoo.com>
2023-05-11 16:49:20 +0300

    Add AssetPersistenceFile to confing

---
+++ finance-project/config/assets.py
@@ -0,0 +1,20 @@
+import json
+
+from config.config import InvalidPersistence
+from persistence.asset_file import AssetPersistenceFile
+from persistence.asset_sqlite import AssetPersistenceSqlite
+
+
+def set_asset_persistence_type(file_path: str):
+    with open(file_path, "r") as f:
+        content = f.read()
+    user_config_choice = json.loads(content)
+    if user_config_choice.get("persistence") == "sqlite":
+        return AssetPersistenceSqlite()
+    elif user_config_choice.get("persistence") == "file":
+        # return AssetPersistenceSqlite()
+        return AssetPersistenceFile("main_assets.json")
+    else:
+        raise InvalidPersistence(
+            "Unknown persistence type, choose between sqlite or file in config.json"
+        )
